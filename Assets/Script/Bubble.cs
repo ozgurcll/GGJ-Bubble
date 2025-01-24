d93757792193a0407bf66d38a862380b11ef787c
Özgür <ozgur.calli52@gmail.com>
2025-01-25 01:58:22 +0300

    ..

---
+++ Assets/Script/Bubble.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [SerializeField] private bool isFulled = false;
+    private float healTimer;
+    private float healInterval = 0.2f;
+
+    private void Start()
+    {
+        healTimer = healInterval;
+    }
+    private void Update()
+    {
+        healTimer -= Time.deltaTime;
+        if (healTimer <= 0 && !isFulled)
+        {
+            healTimer = healInterval;
+            transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+        }
+
+        if (transform.localScale.x >= 2)
+        {
+            isFulled = true;
+        }
+    }
+}
